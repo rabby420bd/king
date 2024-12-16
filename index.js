ed406d59d37f5c3baac82c3546788f4ce6684d98
rabby420bd <63256481+rabby420bd@users.noreply.github.com>
2024-12-16 13:58:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const timingsByCity = require('./api/timingsByCity'); // Import the API logic
+const timingsByCity = require('./api/timingsByCity'); // Import timingsByCity logic
+const emergencyBalance = require('./api/emergencyBalance'); // Import emergencyBalance logic
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,7 +8,10 @@ const port = process.env.PORT || 3000;
 // Route for the prayer timings API
 app.get('/api/timingsByCity', timingsByCity);
 
+// Route for the emergency balance API
+app.get('/api/emergency-balance', emergencyBalance);
+
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
