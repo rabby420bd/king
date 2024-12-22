57f3cd6e9d370507087fbfe27b8501441edc865a
rabby420bd <63256481+rabby420bd@users.noreply.github.com>
2024-12-22 23:31:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const path = require('path');
 const namaz = require('./api/namaz');
 const emergencyBalance = require('./api/emergencyBalance');
 const nagadUserStatus = require('./api/nagadhalf');
-const proxyTsports = require('./api/proxyTsports'); // If you choose proxy method
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -30,14 +29,6 @@ app.get('/livetv', (req, res) => {
 app.get('/toffee', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'toffee.html'));
 });
-
-// Serve the tsports.m3u8 file statically (if placed in 'public' folder)
-app.get('/tsports.m3u8', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'tsports.m3u8'));
-});
-
-// Alternatively, use a proxy method to fetch the M3U8 file dynamically (if you want to proxy from another URL)
-// app.get('/tsports.m3u8', proxyTsports);  // Uncomment this line if you prefer proxying instead
 
 // API routes
 app.get('/api/namaz', namaz);
